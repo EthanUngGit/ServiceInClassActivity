6e3e65963248a737c2e5cf71beea36cd831183e0
EthanUngGit <ethan.ung@temple.edu>
2024-04-16 11:35:49 -0400

    Created menu icons

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -7,17 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import java.util.Timer
 import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
-
-    val handler = android.os.Handler(Looper.getMainLooper()) {
-
-        true
-    }
-
     var timerBinder: TimerService.TimerBinder? = null
 
     val serviceConnection = object: ServiceConnection {
@@ -40,17 +37,44 @@ class MainActivity : AppCompatActivity() {
             BIND_AUTO_CREATE
         )
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
-            timerBinder?.start(100)
+        //findViewById<Button>(R.id.startButton).setOnClickListener {
+            //timerBinder?.start(100)
+        //}
+
+        //findViewById<Button>(R.id.pauseButton).setOnClickListener {
+            //timerBinder?.pause()
+        //}
+
+        //findViewById<Button>(R.id.stopButton).setOnClickListener {
+            //timerBinder?.stop()
+        //}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_start -> {
+                timerBinder?.start(100)
+                Toast.makeText(this, "Starting counter...", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.action_pause -> {
+                timerBinder?.pause()
+                Toast.makeText(this, "Pausing counter...", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.action_stop -> {
+                timerBinder?.stop()
+                Toast.makeText(this, "Stopping counter...", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            timerBinder?.pause()
-        }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
-            timerBinder?.stop()
-        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
